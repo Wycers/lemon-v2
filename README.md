e65591b3356fe51e3087cda3cd62488ba4bd1ee7
wycer <734849165@qq.com>
2018-11-02 16:40:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# lemon-v2
+
+> My magnificent Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn run dev
+
+# build for production and launch server
+$ yarn run build
+$ yarn start
+
+# generate static project
+$ yarn run generate
+```
+
+For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
+# lemon-v2
---
+++ server/index.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const db = 'mongodb://localhost/test'
+
+/**
+ * mongoose连接数据库
+ * @type {[type]}
+ */
+mongoose.Promise = require('bluebird')
+mongoose.connect(db)
+
+/**
+ * 获取数据库表对应的js对象所在的路径
+ * @type {[type]}
+ */
+const models_path = path.join(__dirname, './models')
+
+
+/**
+ * 已递归的形式，读取models文件夹下的js模型文件，并require
+ * @param  {[type]} modelPath [description]
+ * @return {[type]}           [description]
+ */
+var walk = function(modelPath) {
+  fs
+    .readdirSync(modelPath)
+    .forEach(function(file) {
+      var filePath = path.join(modelPath, '/' + file)
+      var stat = fs.statSync(filePath)
+
+      if (stat.isFile()) {
+        if (/(.*)\.(js|coffee)/.test(file)) {
+          require(filePath)
+        }
+      }
+      else if (stat.isDirectory()) {
+        walk(filePath)
+      }
+    })
+}
+walk(models_path)
+
+require('babel-register')
+const Koa = require('koa')
+const CSRF = require('koa-csrf')
+const logger = require('koa-logger')
+const session = require('koa-session')
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
+
+app.keys = ['zhangivon']
+app.use(logger())
+app.use(session(app))
+app.use(bodyParser())
+app.use(new CSRF({
+  invalidSessionSecretMessage: 'Invalid session secret',
+  invalidSessionSecretStatusCode: 403,
+  invalidTokenMessage: 'Invalid CSRF token',
+  invalidTokenStatusCode: 403,
+  excludedMethods: [ 'GET', 'HEAD', 'OPTIONS' ],
+  disableQuery: false
+}));
+
+
+/**
+ * 使用路由转发请求
+ * @type {[type]}
+ */
+const router = require('./route/router')()
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+  
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(app.env === 'production')
+
+async function start() {
+  // Instantiate nuxt.js
+  const nuxt = new Nuxt(config)
+
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  app.use(ctx => {
+    ctx.status = 200 // koa defaults to 404 when it sees that status is unset
+    return new Promise((resolve, reject) => {
+      ctx.res.on('close', resolve)
+      ctx.res.on('finish', resolve)
+      nuxt.render(ctx.req, ctx.res, promise => {
+        // nuxt.render passes a rejected promise into callback on error.
+        promise.then(resolve).catch(reject)
+      })
+    })
+  })
+
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+
+start()
