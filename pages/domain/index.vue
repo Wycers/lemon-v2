f4e727bb217c3374072ab18c8a91b1bf10fa1b9e
wycer <734849165@qq.com>
2018-11-25 12:01:32 +0800

    pug

--- pages/domain/index.vue
+++ pages/domain/index.vue
@@ -1,101 +1,52 @@
-<template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <v-list two-line>
-        <template v-for="(item, index) in domains">
-          <v-subheader
-            v-if="item.header"
-            :key="item.header"
-          >
-            {{ item.header }}
-          </v-subheader>
-
-          <v-divider
-            v-else-if="item.divider"
-            :inset="item.inset"
-            :key="index"
-          />
-
-          <v-list-tile
-            v-else
-            :key="item.title"
-            :to="item.url"
-            avatar
-          >
-            <v-list-tile-avatar>
-              <img :src="item.avatar">
-            </v-list-tile-avatar>
-
-            <v-list-tile-content>
-              <v-list-tile-title v-html="item.title" />
-              <v-list-tile-sub-title v-html="item.subtitle" />
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-flex>
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <v-btn
-        slot="activator"
-        color="red lighten-2"
-        dark
-      >
-        New domain
-      </v-btn>
-      <v-card>
-        <v-card-title
-          class="headline blue"
-          primary-title
-        >
-          A new domain!
-        </v-card-title>
-
-        <v-card-text>
-          <v-form ref="form"> v-model="valid">
-            <v-text-field
+<template lang="pug">
+  v-layout
+    v-flex(text-xs-center)
+      v-list(two-line)
+        template(v-for="(item, index) in domains")
+          v-subheader(v-if="item.header" :key="item.header") {{ item.header }}
+          v-divider(v-else-if="item.divider" :inset="item.inset" :key="index") 
+          v-list-tile(v-else :key="item.title" :to="item.url" avatar)
+            v-list-tile-avatar
+              img(:src="item.avatar")
+            v-list-tile-content
+              v-list-tile-title(v-html="item.title")
+              v-list-tile-sub-title(v-html="item.subtitle")
+    v-dialog(v-model="dialog" width="30%")
+      v-btn(slot="activator" color="red lighten-2" dark) New domain
+      v-card
+        v-card-title.headline.blue(primary-title) A new domain!
+        v-card-text
+          v-form(ref="form" v-model="valid")
+            v-text-field(
               v-model="name"
               :rules="nameRules"
-              :counter="10"
+              :counter="16"
               label="Name"
               required
-            />
-            <v-radio-group v-model="radioGroup">
-              <v-radio
+            )
+            v-radio-group(
+              v-model="radioGroup"
+            )
+              v-radio(
                 v-for="item in types"
                 :key="item.value"
                 :label="item.label"
                 :value="item.value"
                 :disabled="item.disabled"
-              />
-            </v-radio-group>
-          </v-form>  
-        </v-card-text>
-
-        <v-divider/>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
+              )
+        v-divider
+        v-card-actions
+          v-spacer
+          v-btn(
             color="primary"
             flat
             @click="dialog = false"
-          >
-            cancel
-          </v-btn>
-          <v-btn
+          ) cancel
+          v-btn(
             color="primary"
             flat
             @click="dialog = false"
-          >
-            submit
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+          ) submit
 </template>
 <script>
 import http from '../../utils/http'
