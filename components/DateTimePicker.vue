42915e6ae34da0732605d8c801d185d0f4c6c4c3
Wycer <734849165@qq.com>
2019-01-17 20:58:34 +0800

    增加moment更新DateTimePicker

--- components/DateTimePicker.vue
+++ components/DateTimePicker.vue
@@ -1,66 +1,76 @@
 <template lang="pug">
-v-layout(row)
-  div {{ date }} {{ time }}
-  v-menu(
-    :close-on-content-click="false"
-    v-model="dateMenu"
-    :nudge-right="40"
-    lazy
-    transition="fade-transition"
-    offset-y
-  )
-    v-text-field(
-      slot="activator"
-      v-model="date"
-      :label="`${label} - Date`"
-      prepend-icon="event"
-      readonly
-      max-width="50px"
-    )
-    v-date-picker(
-      v-model="date"
-      @input="dateMenu = false"
-    )
+div
+  div {{ date }} {{ time }} {{ utc }}
   v-menu(
     ref="menu"
     :close-on-content-click="false"
-    v-model="timeMenu"
-    :nudge-right="40"
+    v-model="menu"
+    :nudge-right="50"
     :return-value.sync="time"
     lazy
     transition="fade-transition"
     offset-y
+    full-width
+    min-width="290px"
+    max-width="290px"
   )
     v-text-field(
       slot="activator"
-      v-model="time"
-      :label="`${label} - Time`"
-      prepend-icon="access_time"
-      readonly
-      max-width="50px"
+      v-model="display"
+      :label="label"
+      prepend-icon="event"
+      @click="now = true, menu = true"
+    )
+    v-date-picker(
+      v-if="now"
+      v-model="date"
+      @input="now = false"
+      scrollable
+      locale="zh-cn"
     )
     v-time-picker(
+      v-else
       v-model="time"
       @change="$refs.menu.save(time)"
       format="24hr"
+      scrollable
+      locale="zh-cn"
     )
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import moment from 'moment'
 export default Vue.extend({
   props: {
     label: {
+      type: String,
+      required: true
+    },
+    datetime: {
       type: String,
       required: true
     }
   },
   data() {
     return {
-      timeMenu: false,
-      dateMenu: false,
-      date: '',
-      time: ''
+      now: false,
+      menu: false,
+      date: moment(this.datetime).format('YYYY-MM-DD'),
+      time: moment(this.datetime).format('HH:mm')
     }
+  },
+  computed: {
+    display() {
+      return moment(this.date + ' ' + this.time).format('YYYY年MMMDo, HH:mm')
+    },
+    utc() {
+      return moment(this.date + ' ' + this.time)
+        .utc()
+        .format()
+    }
+  },
+  created() {
+    moment.locale('zh-cn')
   }
 })
 </script>
