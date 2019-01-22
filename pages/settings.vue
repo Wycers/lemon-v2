db5d471e0d0c8cb3743f768ef5c128986f29b421
Wycer <734849165@qq.com>
2019-01-22 21:03:13 +0800

    使用新的上传头像模块

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,114 +1,59 @@
-<template>
-  <v-flex 
-    xs12 
-    class="text-xs-center text-sm-center text-md-center text-lg-center"
-  >
-    <img 
-      v-if="imageUrl"
-      :src="imageUrl" 
-      height="150"
-    >
-    <v-text-field
-      v-model="imageName"
-      label="Select Image" 
-      prepend-icon="attach_file"
-      @click="pickFile"
-    />
-    <input
-      ref="image"
-      type="file"
-      style="display: none" 
-      accept="image/*"
-      @change="onFilePicked"
-    >
-  </v-flex>
+<template lang="pug">
+cuavatar(
+  :src="avatar"
+  size=128
+  @crop-success="cropSuccess"
+  @crop-upload-success="cropUploadSuccess"
+  @crop-upload-fail="cropUploadFail"
+)
 </template>
+
 <script>
 import axios from 'axios'
-
+import { mapState } from 'vuex'
+import CUAvatar from '~/components/CUAvatar'
 export default {
-  data() {
-    return {
-      title: 'Image Upload',
-      dialog: false,
-      imageName: '',
-      imageUrl: '',
-      imageFile: ''
-    }
+  components: {
+    cuavatar: CUAvatar
   },
   fetch({ store, redirect }) {
     if (!store.state.auth.token) {
       return redirect('/')
     }
   },
+  data() {},
+  computed: {
+    ...mapState({
+      nickname: state => state.auth.nickname,
+      avatar: state => state.auth.avatar,
+      token: state => state.auth.token
+    })
+  },
   methods: {
-    pickFile() {
-      this.$refs.image.click()
+    cropSuccess(imgDataUrl, field) {
+      console.log('-------- crop success --------')
+      this.imgDataUrl = imgDataUrl
     },
-    uploadInputchange() {
-      let file = document.getElementById('uploadFileInput').files[0] // 选择的图片文件
-      this.uploadImgToQiniu(file)
-    },
-    // 上传图片到七牛
-    async uploadImgToQiniu(file) {
-      const res = await axios.post('/api/qiniu/upload', {
-        token: this.$store.state.token
-      })
-      console.log(res)
-      const axiosInstance = axios.create({ withCredentials: false })
-      // withCredentials 禁止携带cookie，带cookie在七牛上有可能出现跨域问题
-      let data = new FormData()
-      data.append('token', res.data.token)
-      data.append('key', res.data.key)
-      data.append('file', file)
-      axiosInstance({
-        method: 'POST',
-        url: 'http://upload-z2.qiniup.com', //上传地址
-        data: data,
-        // 超时时间，因为图片上传有可能需要很久
-        onUploadProgress: progressEvent => {
-          //imgLoadPercent 是上传进度，可以用来添加进度条
-          let imgLoadPercent = Math.round(
-            (progressEvent.loaded * 100) / progressEvent.total
-          )
-          console.log(imgLoadPercent)
-        }
-      })
-        .then(res => {
-          console.log(res)
-          if (res.data.success === true) {
-            try {
-              this.$store.commit('auth/SET_USER', res.data.user)
-            } catch (error) {
-              alert('failed')
-            }
-          } else {
-            alert('failed')
-          }
-        })
-        .catch(err => {
+    cropUploadSuccess(data, field) {
+      console.log('-------- upload success --------')
+      console.log(data)
+      console.log('field: ' + field)
+      if (data.success === true) {
+        try {
+          this.$store.commit('auth/SET_USER', {
+            avatar: data.url
+          })
+        } catch (error) {
           alert('failed')
-        })
-    },
-    onFilePicked(e) {
-      const files = e.target.files
-      if (files[0] !== undefined) {
-        this.imageName = files[0].name
-        if (this.imageName.lastIndexOf('.') <= 0) {
-          return
         }
-        const fr = new FileReader()
-        fr.readAsDataURL(files[0])
-        fr.addEventListener('load', () => {
-          this.imageUrl = fr.result
-          this.imageFile = files[0] // this is an image file that can be sent to server...
-          this.uploadImgToQiniu(files[0])
-        })
       } else {
-        this.imageName = ''
-        this.imageFile = ''
-        this.imageUrl = ''
+        alert('failed')
       }
+    },
+    cropUploadFail(status, field) {
+      console.log('-------- upload fail --------')
+      console.log(status)
+      console.log('field: ' + field)
     }
   }
 }
